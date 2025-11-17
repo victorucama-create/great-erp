8d7c2533bedcc4ab46a6ab85fd59f652e7cedcde
victorucama-create <victorucama@gmail.com>
2025-11-17 17:16:04 +0200

    Update products.routes.js

--- backend/src/routes/products.routes.js
+++ backend/src/routes/products.routes.js
@@ -1,46 +1,17 @@
-// src/routes/products.routes.js
 const express = require('express');
-const { body, param } = require('express-validator');
 const router = express.Router();
-const productsController = require('../controllers/products.controller');
-const { verifyToken } = require('../middlewares/auth.middleware');
-const upload = require('../middlewares/upload.middleware');
+const productsCtrl = require('../controllers/products.controller');
+const { verifyToken } = require('../middlewares/auth');
+const { tenantFilter } = require('../middlewares/tenant');
+const upload = require('../middlewares/upload');
 
-// Public list (could be tenant-filtered)
-router.get('/products', verifyToken, productsController.listProducts);
-router.get('/products/:id', verifyToken, [ param('id').isUUID() ], productsController.getProduct);
+router.get('/products', verifyToken, tenantFilter, productsCtrl.listProducts);
+router.get('/products/:id', verifyToken, tenantFilter, productsCtrl.getProduct);
+router.post('/products', verifyToken, tenantFilter, productsCtrl.createProduct);
+router.put('/products/:id', verifyToken, tenantFilter, productsCtrl.updateProduct);
+router.delete('/products/:id', verifyToken, tenantFilter, productsCtrl.deleteProduct);
+router.post('/products/:id/attachments', verifyToken, tenantFilter, upload.single('file'), productsCtrl.uploadAttachment);
 
-// CRUD
-router.post('/products',
-  verifyToken,
-  [
-    body('sku').isString().notEmpty(),
-    body('name').isString().notEmpty(),
-    body('salePrice').isNumeric()
-  ],
-  productsController.createProduct
-);
-
-router.put('/products/:id',
-  verifyToken,
-  [ param('id').isUUID() ],
-  productsController.updateProduct
-);
-
-router.delete('/products/:id',
-  verifyToken,
-  [ param('id').isUUID() ],
-  productsController.deleteProduct
-);
-
-// Attachments
-router.post('/products/:id/attachments', verifyToken, upload.single('file'), productsController.uploadAttachment);
-
-// Stock movements / inventory
-router.post('/stock-movements', verifyToken, productsController.createStockMovement);
-router.get('/stock/:productId', verifyToken, productsController.getStockByProduct);
-
-// export CSV
-router.get('/products-export/csv', verifyToken, productsController.exportProductsCsv);
+router.post('/stock-movements', verifyToken, tenantFilter, productsCtrl.createStockMovement);
 
 module.exports = router;
