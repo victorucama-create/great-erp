3da1bc3e1f0d28edf947ad5047adef7afdf05aa7
victorucama-create <victorucama@gmail.com>
2025-11-17 18:12:38 +0200

    Update index.js

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 router.use('/auth', require('./auth.routes'));
 router.use('/erp', require('./products.routes'));
 router.use('/mola', require('./mola.routes'));
+router.use('/erp/sales', require('./sales.routes'));
 
 // TODO: add crm, mrp, hr, b2b, logistics routes
 
