b31829e1fb9a412326a70aca61975a265a9cb7ea
victorucama-create <victorucama@gmail.com>
2025-11-17 18:55:18 +0200

    Update tenant.js

--- backend/src/middlewares/tenant.js
+++ backend/src/middlewares/tenant.js
@@ -1,18 +1,8 @@
-// middlewares/tenant.js
-// Ensures tenant isolation: sets req.tenantFilter for controllers
-function tenantFilter(req, res, next) {
-  const user = req.user;
-  if (!user) {
-    req.tenantFilter = {};
-    return next();
-  }
-  // super_admin can see all tenants; tenant_admin sees own tenant
-  if (user.role === 'super_admin') {
-    req.tenantFilter = {}; // no filtering
-  } else {
-    req.tenantFilter = { tenantId: user.tenantId };
-  }
-  next();
-}
-
-module.exports = { tenantFilter };
+module.exports.tenantFilter = (req, res, next) => {
+    if (req.user.role === "super_admin") {
+        req.tenantFilter = {};
+    } else {
+        req.tenantFilter = { tenantId: req.user.tenantId };
+    }
+    next();
+};
